2d2e394f0b01e49b00016824657fb3c9aa0c98d1
insu97 <52162653+insu97@users.noreply.github.com>
2022-10-02 16:48:54 +0900

    add chapter3

--- 001)초기세팅.py
+++ 001)초기세팅.py
@@ -1,12 +1,13 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QMainWindow, QAction, qApp
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QMainWindow, QAction, qApp, QDesktopWidget
 from PyQt5.QtGui import QIcon, QFont
-from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtCore import QCoreApplication, QDate, Qt
 
 class MyApp(QMainWindow) : # QWidget : 기본, QMainWindow : 메인창
 
     def __init__(self):
         super().__init__()
+        self.date = QDate.currentDate()
         self.initUI()
 
     def initUI(self):
@@ -15,16 +16,20 @@ class MyApp(QMainWindow) : # QWidget : 기본, QMainWindow : 메인창
         QToolTip.setFont(QFont('SansSerif', 10))
 
         # 타이틀 제목, 아이콘
-        self.setWindowTitle('My First Application & Icon & Quit Button')
+        self.setWindowTitle('My First Application')
         self.setWindowIcon(QIcon('img/eng.png'))
 
         # 상태바 만들기
-        self.statusBar().showMessage('Ready')
+        ## self.statusBar().showMessage('Ready')
+
+        ## 상태 표시줄에 날짜 표시하기
+        self.statusBar().showMessage(self.date.toString(Qt.DefaultLocaleLongDate))
 
         # 윈도우 창 만들기
         ## self.move(300, 300)
         ## self.resize(400, 200)
-        self.setGeometry(300, 300, 300, 200) # 위치 : (x , y) , 창 : (너비 , 높이)
+        self.setGeometry(300, 300, 400, 200) # 위치 : (x , y) , 창 : (너비 , 높이)
+        self.center()
 
         ## 종료 버튼 만들기
         btn = QPushButton('Quit', self) # 문구설정
@@ -53,6 +58,11 @@ class MyApp(QMainWindow) : # QWidget : 기본, QMainWindow : 메인창
 
         self.show()
 
+    def center(self):
+        qr = self.frameGeometry() # 창의 위치와 크기 정보 가져오기
+        cp = QDesktopWidget().availableGeometry().center() # 사용하는 모니터 화면의 가운데 위치를 파악
+        qr.moveCenter(cp) # 가운데로 이동
+        self.move(qr.topLeft())
 
 if __name__ == '__main__' :
     app = QApplication(sys.argv)
